2c4203529034addca56d6d0e7ce29b051a432da9
dell <1332011796@qq.com>
2023-08-30 20:57:05 +0800

    slightly modify

--- reverse_docking/lirmsd_analysis.py
+++ reverse_docking/lirmsd_analysis.py
@@ -19,24 +19,12 @@ Should be used right after `ligand_rmsd.py`.
 
 
 parser = argparse.ArgumentParser(description="Generate ligand and pocket RMSD by CSV.")
-parser.add_argument("-i", "--input", required=True, help="Input JSON file name")
+parser.add_argument("-i", "--input",required=True, help="Input JSON file name")
 parser.add_argument("-o", "--output", default="test.csv", help="Output CSV file name")
-parser.add_argument(
-    "--order",
-    choices=["best", "worst", "mean", "pocket"],
-    default="sample_number",
-    help="Order results by highest, lowest, or mean affinity",
-)
-parser.add_argument(
-    "-t", "--threshold", type=float, help="Threshold for mean affinity filtering"
-)
-parser.add_argument(
-    "-q",
-    "--quantile",
-    type=int,
-    choices=[25, 50, 75],
-    help="Quantile value for affinity values",
-)
+parser.add_argument("--order", choices=["best", "worst", "mean", "pocket"], default="sample_number",
+                    help="Order results by highest, lowest, or mean affinity")
+parser.add_argument("-t", "--threshold", type=float, help="Threshold for mean affinity filtering")
+parser.add_argument("-q", "--quantile", type=int, choices=[25, 50, 75], help="Quantile value for affinity values")
 args = parser.parse_args()
 
 # Load the JSON data
@@ -49,6 +37,7 @@ best_RMSD = []
 worst_RMSD = []
 mean_RMSD = []
 pocket_RMSD = []
+pose1_RMSD = []
 
 # Iterate through the data and calculate the metrics
 for sample_number, sample_data in data.items():
@@ -57,24 +46,24 @@ for sample_number, sample_data in data.items():
     worst_rmsd = max(lirmsd)
     mean_rmsd = sample_data["mean_Ligand_RMSD"]
     pocket_rmsd = sample_data["Pocket_RMSD"]
+    pose1_rmsd = sample_data["poses"]["pose_1"]
 
     sample_numbers.append(sample_number)
     best_RMSD.append(best_rmsd)
     worst_RMSD.append(worst_rmsd)
     mean_RMSD.append(mean_rmsd)
     pocket_RMSD.append(pocket_rmsd)
+    pose1_RMSD.append(pose1_rmsd)
 
 
-df = pd.DataFrame(
-    {
-        "Sample Number": sample_numbers,
-        "Best ligand-RMSD": best_RMSD,
-        "Wrost ligand-RMSD": worst_RMSD,
-        "Mean ligand-RMSD": mean_RMSD,
-        "Pocket-RMSD": pocket_RMSD,
-    }
-)
-
+df = pd.DataFrame({
+    "Sample Number": sample_numbers,
+    "Best ligand-RMSD": best_RMSD,
+    "Pose1 RMSD": pose1_RMSD,
+    "Wrost ligand-RMSD": worst_RMSD,
+    "Mean ligand-RMSD": mean_RMSD,
+    "Pocket-RMSD": pocket_RMSD
+})
 
 def quantile_calculate(quantile):
     all_mean_RMSD = []
@@ -84,8 +73,8 @@ def quantile_calculate(quantile):
 
     for sample_data in data.values():
         all_mean_RMSD.append(sample_data["mean_Ligand_RMSD"])
-        all_best_RMSD.extend(sample_data["poses"].values())  # Something wrong here
-        all_worst_RMSD.extend(sample_data["poses"].values())  # Something wrong here
+        all_best_RMSD.extend(sample_data["poses"].values()) #Something wrong here
+        all_worst_RMSD.extend(sample_data["poses"].values()) #Something wrong here
         all_pocket_RMSD.append(sample_data["Pocket_RMSD"])
 
     mean_quantile = np.percentile(all_mean_RMSD, quantile)
@@ -97,8 +86,7 @@ def quantile_calculate(quantile):
     print(f"Highest ligand-RMSD of {quantile}th percentile: {best_quantile:.3f}.")
     print(f"Lowest ligand-RMSD of {quantile}th percentile: {worst_quantile:.3f}.")
     print(f"Pocket-RMSD of {quantile}th percentile: {pocket_quantile:.3f}")
-
-
+    
 # Sort the DataFrame based on the specified order
 if args.order == "best":
     df = df.sort_values(by="Best ligand-RMSD", ascending=True)
